7f192993e3929d100d1c03dc9bc3de5954afa6b6
Remeto <remetojohn@gmail.com>
2019-10-16 16:46:45 -0700

    moved stuff

--- server.js
+++ server.js
@@ -18,13 +18,6 @@ const MONGODB_URI =
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.post('/api/articles', (req, res) => {
   article
     .create(req.body)
@@ -69,6 +62,12 @@ app.post('/api/postnotes', (req, res) => {
     });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // Handles any requests that don't match the ones above
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
